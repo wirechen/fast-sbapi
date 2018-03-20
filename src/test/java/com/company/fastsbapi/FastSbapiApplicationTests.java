a00d7a1bac0d36d2dba9047a5e0045560b130367
wirechen <wirechen@gmail.com>
2018-03-20 15:42:37 +0800

    FastSbAPI v1.0版本

---
+++ src/test/java/com/company/fastsbapi/FastSbapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.fastsbapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FastSbapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
